c8dbaf5b22d8fc263565fa3516e92b4f3c0d4afc
Marlena Franckiewicz <s14746@pjwstk.edu.pl>
2018-06-17 12:49:01 +0200

    before creating DB

---
+++ Projekt1/Data/Projekt1Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Projekt1.Models
+{
+    public class Projekt1Context : DbContext
+    {
+        public Projekt1Context (DbContextOptions<Projekt1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projekt1.Models.Book> Book { get; set; }
+    }
+}
---
+++ Projekt1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt1.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public decimal PublicationDate { get; set; }
+    }
+}
